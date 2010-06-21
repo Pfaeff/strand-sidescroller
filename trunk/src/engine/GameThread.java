59b9523bd644442dcb8f12c06b6e010431aac019
roehr@informatik.uni-luebeck.de <roehr@informatik.uni-luebeck.de@66383b5e-27b6-3a86-b64c-052263a8f6bc>
2010-06-21 15:57:02 +0000

    Übersichtlichere Single-Thread Struktur

---
+++ trunk/src/engine/GameThread.java
@@ -0,0 +1,37 @@
+package engine;
+
+import game.Game;
+
+import javax.media.opengl.GLCanvas;
+
+final public class GameThread extends Thread {
+	private boolean runs = true;
+	final private Game game;
+	final private GLCanvas canvas;
+
+	public GameThread(Game game, GLCanvas canvas) {
+		super();
+		this.game = game;
+		this.canvas = canvas;
+	}
+
+	final public void run() {
+		while (runs) {
+			game.updateGame();
+			canvas.display();
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public synchronized void endGameLoop() {
+		runs = false;
+	}
+
+	public synchronized boolean isRunning() {
+		return runs;
+	}
+}
