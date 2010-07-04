dc8d8c16cecbd218ebb579802e3fbdefa5f38594
roehr@informatik.uni-luebeck.de <roehr@informatik.uni-luebeck.de@66383b5e-27b6-3a86-b64c-052263a8f6bc>
2010-07-04 17:37:19 +0000

    Erste Version des Animationssystems (ungetestet)

--- trunk/src/render/Animation.java
+++ trunk/src/render/Animation.java
@@ -9,6 +9,8 @@ public class Animation {
 	private int duration;
 	private int numX;
 	private int numY;
+	private int posX;
+	private int posY;
 	private boolean loop;
 	private float position;
 	
@@ -18,22 +20,37 @@ public class Animation {
 		this.numX = numX;
 		this.numY = numY;
 		this.loop = loop;
-		position = 0;
+		reset();
 	}
 	
 	public void reset() {
 		position = 0;
+		posX = 0;
+		posY = 0;		
 	}
 	
 	public void update(float dt) {
-		
+		position += 1000 * dt;
+		if ((position >= duration) && (loop)) {
+			reset();
+		} else {
+			float dp = duration / position;
+			posX = (int)Math.ceil(dp / numX);		
+			posY = (int)Math.ceil(dp / numY);
+		}		
 	}
 	
 	public void render(GL gl) {
 		texture.bind();
+		int tX = numX / posX;
+		int tY = numY / posY;		
 		gl.glBegin(GL.GL_QUADS);
 		{
-			
+			gl.glScalef(0.5f, 0.5f, 1f);
+			gl.glTexCoord2d( 0, tY); gl.glVertex3f(-1, -1, 0);  
+			gl.glTexCoord2d(tX, tY); gl.glVertex3f( 1, -1, 0);
+			gl.glTexCoord2d(tX,  0); gl.glVertex3f( 1,  1, 0);
+			gl.glTexCoord2d( 0,  0); gl.glVertex3f(-1,  1, 0);			
 		}
 		gl.glEnd();
 	}
